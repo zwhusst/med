ac2a5526548e42a56df4b9a56d9426e336d60610
zwhusst <zwhusst@gmail.com>
2012-04-29 11:51:02 +0800

    restrict web search

--- medweb/src/web/medweb_search.js
+++ medweb/src/web/medweb_search.js
@@ -3,9 +3,9 @@
   var box_label = 'Search for Health Information';
   var box_width = 165;
   var html = [
-    '<form method="post" target="_blank" action="'+site+'/s" onsubmit="javascirpt:this.service.value=window.location; this.action=\''+site+'/rs/ss/\'+encodeURI(this.qalias.value)+'+'\'?qs=\''+'+encodeURI(this.qalias.value)'+';return true;">',
+    '<form method="post" target="_blank" action="'+site+'/s" onsubmit="javascirpt:this.outersite.value=window.location; this.action=\''+site+'/rs/ss/\'+encodeURI(this.qalias.value)+'+'\'?qs=\''+'+encodeURI(this.qalias.value)'+';return true;">',
     '<div style="font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: bold; color: #000000">'+box_label+'</div>',
-    '<input type="hidden" name="service" value="external" />',
+    '<input type="hidden" name="outersite" value="external" />',
     '<input style="vertical-align: middle; width: '+box_width+'px" type="text" name="qalias" maxlength="127" value="" />',
     '&nbsp;',
     '<input style="vertical-align: middle; width: 30px" type="submit" value="Go" />',
--- medweb/src/web/medweb_search_narrow.js
+++ medweb/src/web/medweb_search_narrow.js
@@ -1,11 +1,11 @@
 ﻿(function(){
-  var site = 'http://www.ttdong.com.cn/medweb';
+  var site = 'http://localhost:8980/medweb';
   var box_label = 'Health Search';
   var box_width = 80;
   var html = [
-    '<form method="post" target="_blank" action="'+site+'/s" onsubmit="javascirpt:this.service.value=window.location; this.action=\''+site+'/rs/ss/\'+encodeURI(this.qalias.value)+'+'\'?qs=\''+'+encodeURI(this.qalias.value)'+';return true;">',
+    '<form method="post" target="_blank" action="'+site+'/s" onsubmit="javascirpt:this.outersite.value=window.location; this.action=\''+site+'/rs/ss/\'+encodeURI(this.qalias.value)+'+'\'?qs=\''+'+encodeURI(this.qalias.value)'+';return true;">',
     '<div style="font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: bold; color: #000000">'+box_label+'</div>',
-    '<input type="hidden" name="service" value="external" />',
+    '<input type="hidden" name="outersite" value="external" />',
     '<input style="vertical-align: middle; width: '+box_width+'px" type="text" name="qalias" maxlength="127" value="" />',
     '&nbsp;',
     '<input style="vertical-align: middle; width: 30px" type="submit" value="Go" />',
